6c5fa6a0462a19bcca93308fdc84dc0b86775d92
BryceGreenleaf <brycegreenleaf25@gmail.com>
2023-11-16 20:25:31 -0700

    Make the design

--- Robot/Form1.Designer.cs
+++ Robot/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            panel1 = new Panel();
+            lblRobot = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblRobot);
+            panel1.Location = new Point(31, 31);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(220, 220);
+            panel1.TabIndex = 1;
+            // 
+            // lblRobot
+            // 
+            lblRobot.BackColor = SystemColors.ControlLight;
+            lblRobot.Font = new Font("Wingdings", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblRobot.ForeColor = SystemColors.ActiveCaptionText;
+            lblRobot.Location = new Point(100, 100);
+            lblRobot.Margin = new Padding(2, 0, 2, 0);
+            lblRobot.Name = "lblRobot";
+            lblRobot.Size = new Size(20, 20);
+            lblRobot.TabIndex = 0;
+            lblRobot.Text = "a";
+            lblRobot.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 271);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(131, 271);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(176, 300);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(131, 329);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 7;
+            label2.Text = "{X=0,Y=0}";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(284, 376);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Robot";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label lblRobot;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
     }
 }
